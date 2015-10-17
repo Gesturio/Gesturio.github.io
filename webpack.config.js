f9770511a2731486ba5bfb57bf8acd979a3aa657
Aitem <muradbei@gmail.com>
2015-10-17 22:20:19 +0400

    Webpack base setting

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require("path");
+var etx = require("extract-text-webpack-plugin");
+module.exports = {
+    context: __dirname,
+    entry: "./src/app.js",
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "app.js"
+    },
+    module: {
+        loaders: [
+            { test: "\.js$", loader: "file-loader" },
+            { test: /\.coffee$/, loader: "coffee-loader" },
+            { test: /\.less$/,   loader: etx.extract("style-loader","css-loader?minimize!postcss-loader!less-loader")},
+            { test: /\.css$/,    loader: etx.extract("style-loader", "css-loader?minimize!") }
+        ]
+    }
+};
