fef0888e2273f58d4d67609532b56efd295bfcd9
Aitem <muradbei@gmail.com>
2015-10-21 23:02:34 +0400

    clear gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,10 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var assetRev = require('gulp-asset-rev-jade');
-var assets = require("gulp-assets");
 var rev = require('gulp-rev');
-var webserver = require('gulp-webserver');
-var st = require('st');
-var lr = require('tiny-lr');
-var lrserver = lr();
 var ecstatic = require('ecstatic');
-
-var livereloadport = 3001,
-    serverport = 8000;
-
-var refresh = require('gulp-livereload'),
-    http = require('http');
+var serverport = 8000;
+var  http = require('http');
 
 gulp.task('build', function() {
     gulp.src('./src/img/*')
@@ -24,7 +15,6 @@ gulp.task('build', function() {
         .pipe(jade())
         .pipe(assetRev({connecter: '-'}))
         .pipe(gulp.dest('./dist/'))
-        .pipe(refresh(lrserver))
 });
 
 gulp.task('watch', function() {
@@ -33,7 +23,5 @@ gulp.task('watch', function() {
 
 gulp.task('server', ['watch'], function() {
     http.createServer(ecstatic({ root: __dirname + '/dist', index: "index.html" })).listen(serverport);
-
-    lrserver.listen(livereloadport);
 });
 
